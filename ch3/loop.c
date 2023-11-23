d57f6c852a3e5b9411a66f289efe910f7c5996e1
Truter21 <sjso22@naver.com>
2023-11-23 20:52:50 +0900

    feat : loop

---
+++ ch3/loop.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+int main(void)
+{
+  // int b = 20;
+  // printf("b는 %d\n", ++b);
+  // printf("b는 %d\n", b++);
+  // printf("b는 %d\n", b);
+  // return 0;
+
+  // for 문
+  // for (int i = 1; i <= 10; i++)
+  // {
+  //   printf("Hello World!\n");
+  // }
+  // return 0;
+
+  // while 문
+  // int i = 1;
+  // while (i<=10)
+  // {
+  //   printf("Hello World! %d\n", i++);
+  // }
+  // return 0;
+  
+  // do-while 문
+  // int i = 1;
+  // do
+  // {
+  //   printf("Hello World! %d\n", i++);
+  // } while (i <= 10);
+  // return 0;
+
+  // 이중 반복문
+  // for (int i = 1; i <= 3; i++)
+  // {
+  //   printf("첫 번째 반복문 : %d\n", i);
+  //   for (int j = 1; j <= 5; j++)
+  //   {
+  //     printf("    두 번째 반복문 : %d\n", j);
+  //   }
+  // }
+  // return 0;
+
+  // 실습 1: 구구단 출력하기
+  // for (int i = 2; i <= 9; i++)
+  // {
+  //   printf("%d단 출력\n", i);
+  //   for (int j = 1; j <= 9; j++)
+  //   {
+  //     printf("%d x %d = %d\n", i, j, i*j);
+  //   }
+  // }
+  // return 0;
+
+  // 실습 2: 별표 출력하기
+  // for (int i = 1; i <= 5; i++)
+  // {
+  //   for (int j = 1; j <= i; j++)
+  //   {
+  //     printf("*");
+  //   }
+  //   printf("\n");
+  // }
+  // return 0;
+
+  // for (int i = 0; i < 5; i++)
+  // {
+  //   for (int j = i; j <= 4; j++)
+  //   {
+  //     printf(" ");
+  //   }
+  //   for (int k = 0; k <= i; k++)
+  //   {
+  //     printf("*");
+  //   }
+  //   printf("\n");
+  // }
+  // return 0;
+}
