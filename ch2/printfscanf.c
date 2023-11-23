4cd05da337b3f7130111d340739f1e4b5dfeb866
Truter21 <sjso22@naver.com>
2023-11-23 20:52:37 +0900

    feat : printf scanf

---
+++ ch2/printfscanf.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void)
+{
+  /* int age = 20;
+  printf("%d\n", age);
+  age = 21;
+  printf("%d\n", age); */
+
+  // printf("1\n");
+  // printf("2\n");
+  // printf("3\n");
+
+  /* float f =46.5f;
+  printf("%.2f\n", f);
+  double d =4.428;
+  printf("%.2lf\n", d); */
+
+  // Quiz.1
+  // float f = 2121.1020f;
+  // printf("%.3f\n", f);
+
+  // int add = 3 + 7;
+  // printf("3 + 7 = %d\n", add);
+  // printf("%d + %d = %d\n", 3, 7, 3 + 7);
+
+  // int input;
+  // printf("값을 입력하세요 : ");
+  // scanf_s("%d", &input);
+  // printf("입력값 : %d\n", input);
+
+  // char c = 'A';
+  // printf("%c\n", c);
+
+  // char str[256];
+  // scanf_s("%s", str, sizeof(str));
+  // printf("%s\n", str);
+
+  // char str1[256], str2[256];
+  // printf("첫 번째 문자열을 입력하세요 : ");
+  // scanf_s("%s", str1, sizeof(str1));
+  // printf("두 번째 문자열을 입력하세요 : ");
+  // scanf_s("%s", str2, sizeof(str2));
+  // printf("첫 번째 문자열은 %s입니다.\n", str1);
+  // printf("두 번째 문자열은 %s입니다.\n", str2);
+  return 0;
+}
